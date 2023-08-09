3dae69fc5ca0982447905164e76b482b755b6216
Loh Silva <lo-ruama_sp@hotmail.com>
2023-08-08 21:09:40 -0300

    Subtotal and Total methods

--- src/main/java/com/projetospringboot/course/entities/Order.java
+++ src/main/java/com/projetospringboot/course/entities/Order.java
@@ -97,11 +97,16 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}return sum;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
